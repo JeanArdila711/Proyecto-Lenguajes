5f7f32b2f2b26332ed0be777f890d9f54303ec0e
JeanArdila711 <125698754+JeanArdila711@users.noreply.github.com>
2023-11-21 21:06:25 -0500

    Update mierda.py

--- mierda.py
+++ mierda.py
@@ -3,85 +3,108 @@ from PIL import Image, ImageTk
 import re
 import os
 
+
 # emojis y sus rutas de imagen correspondientes
-emoji_a_imagen = {
-    ":)": os.path.abspath('emoji_parser-main/emojicshd/png/005-sonriente.png'), 
-    ":3": os.path.abspath('emoji_parser-main/emojicshd/png/063-perro-3.png'),
-    "xD": os.path.abspath('emoji_parser-main/emojicshd/png/010-risa.png'),
-    ":\(": os.path.abspath('emoji_parser-main/emojicshd/png/009-triste.png'),
-    ":D": os.path.abspath('emoji_parser-main/emojicshd/png/058-riendo.png'),
-    ";\)": os.path.abspath('emoji_parser-main/emojicshd/png/018-guino.png'),
-    ":-\)": os.path.abspath('emoji_parser-main/emojicshd/png/006-feliz-1.png'),
-    "(y)": os.path.abspath('emoji_parser-main/emojicshd/png/031-me-gusta-1.png'),
-    "(n)": os.path.abspath('emoji_parser-main/emojicshd/png/028-pulgares-abajo.png'),
-    "<3": os.path.abspath('emoji_parser-main/emojicshd/png/067-corazon.png'),
-    ":-O": os.path.abspath('emoji_parser-main/emojicshd/png/055-sorpresa.png'),
-    ":\(": os.path.abspath('emoji_parser-main/emojicshd/png/009-triste.png'),
-    ":O": os.path.abspath('emoji_parser-main/emojicshd/png/004-conmocionado.png'),
-    ":/": os.path.abspath('emoji_parser-main/emojicshd/png/008-confuso.png'),
-    ":*": os.path.abspath('emoji_parser-main/emojicshd/png/068-beso.png'),
-    "\^\^": os.path.abspath('emoji_parser-main/emojicshd/png/016-estrella.png'),
-    "·--·": os.path.abspath('emoji_parser-main/emojicshd/png/069_gato.png')
+EMOJI_A_IMAGEN = {
+    ":)": os.path.abspath("emoji_parser-main\emojicshd\png/005-sonriente.png"),
+    ":3": os.path.abspath("emoji_parser-main\emojicshd\png/063-perro-3.png"),
+    "xD": os.path.abspath("emoji_parser-main\emojicshd\png/010-risa.png"),
+    ":(": os.path.abspath("emoji_parser-main\emojicshd\png/009-triste.png"),
+    ":D": os.path.abspath("emoji_parser-main\emojicshd\png/058-riendo.png"),
+    ";)": os.path.abspath("emoji_parser-main\emojicshd\png/018-guino.png"),
+    ":-)": os.path.abspath("emoji_parser-main\emojicshd\png/006-feliz-1.png"),
+    "(y)": os.path.abspath("emoji_parser-main\emojicshd\png/031-me-gusta-1.png"),
+    "(n)": os.path.abspath("emoji_parser-main\emojicshd\png/028-pulgares-abajo.png"),
+    "<3": os.path.abspath("emoji_parser-main\emojicshd\png/067-corazon.png"),
+    ":-O": os.path.abspath("emoji_parser-main\emojicshd\png/055-sorpresa.png"),
+    ":\(": os.path.abspath("emoji_parser-main\emojicshd\png/009-triste.png"),
+    ":O": os.path.abspath("emoji_parser-main\emojicshd\png/004-conmocionado.png"),
+    ":/": os.path.abspath("emoji_parser-main\emojicshd\png/008-confuso.png"),
+    "·--·": os.path.abspath("emoji_parser-main\emojicshd\png/069_gato.png"),
 }
+
 # expresión regular para identificar emojis
-regex_emojis = r":3|xD|:\)|:\(|:D|;\)|:P|:-\)|:-\(|\(y\)|\(n\)|<3|:-O|:O|:-/|:/|:*|>:\(|\^\^|:-\]|·--·"
+REGEX_EMOJIS =  r":3|xD|:\)|:\(|:D|;\)|:-\)|\(y\)|\(n\)|<3|:-O|:O|:/|·--·"
+
 
 class VerificadorPalabras:
     def __init__(self, ruta_diccionario):
-        self.palabras_espanol = self.cargar_diccionario(ruta_diccionario)
+        self.ruta_diccionario = ruta_diccionario
+        self.palabras_espanol = self.cargar_diccionario()
 
-    def cargar_diccionario(self, ruta_diccionario):
-        diccionario_palabras_espanol = set()
-        with open(ruta_diccionario, 'r', encoding='utf-8') as archivo_diccionario:
-            for linea in archivo_diccionario:
-                palabra = linea.strip()
-                diccionario_palabras_espanol.add(palabra.lower())
-        return diccionario_palabras_espanol
+    def cargar_diccionario(self):
+        try:
+            with open(self.ruta_diccionario, "r", encoding="utf-8") as archivo:
+                palabras = archivo.read().splitlines()
+                return set(palabras)
+        except FileNotFoundError:
+            print("El archivo del diccionario no se encontró.")
+            return set()
 
-    def es_palabra_espanol(self, palabra):
-        return palabra.lower() in self.palabras_espanol
+    def contar_palabras_espanol(self, texto):
+        palabras = texto.split(' ')
+        palabras_espanol = 0
+        for palabra in palabras:
+            palabra_limpia = palabra.strip()
+            if palabra_limpia.isalpha() and not re.match(REGEX_EMOJIS, palabra_limpia):
+                palabras_espanol += 1
+        return palabras_espanol
+
 
 def identificar_emojis(texto):
-    return re.findall(regex_emojis, texto)
+    lista_emojis = re.findall(REGEX_EMOJIS, texto)
+    return lista_emojis, len(lista_emojis)
 
-def mostrar_resultados(entrada, palabras_encontradas, total_emojis):
-    etiqueta_texto.config(text=entrada)
-    etiqueta_salida.config(text=f"Se encontraron {palabras_encontradas} palabras en español y {total_emojis} emojis.")
 
-def analizador_lexicografico(entrada):
-    lista_emojis_encontrados = identificar_emojis(entrada)
-    entrada_con_emojis = reemplazar_palabra_con_emoji(entrada, lista_emojis_encontrados)
-
-    verificador = VerificadorPalabras('emoji_parser-main/diccionario.txt')
-    palabras_encontradas = 0
-    palabras = re.findall(r'\b[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+\b', entrada)
-    for palabra in palabras:
-        if verificador.es_palabra_espanol(palabra.lower()):
-            palabras_encontradas += 1
-
-    total_emojis = len(lista_emojis_encontrados)
-    mostrar_resultados(entrada_con_emojis, palabras_encontradas, total_emojis)
-
-def reemplazar_palabra_con_emoji(entrada, lista_emojis_encontrados):
+def reemplazar_emojis_con_imagenes(entrada, lista_emojis_encontrados):
     texto_con_emojis = entrada
-    emojis_frame = tk.Frame(ventana)
-    emojis_frame.pack()
+    emojis = 0
 
     for emoji_encontrado in lista_emojis_encontrados:
-        ruta_imagen = emoji_a_imagen.get(emoji_encontrado)
+        ruta_imagen = EMOJI_A_IMAGEN.get(emoji_encontrado)
         if ruta_imagen:
             imagen = Image.open(ruta_imagen)
-            imagen = imagen.resize((40, 40), Image.LANCZOS)
+            imagen = imagen.resize(
+                (40, 40), Image.LANCZOS
+            )  # Ajusta el tamaño del emoji a 30x30 píxeles con filtro LANCZOS
             imagen = ImageTk.PhotoImage(imagen)
 
-            etiqueta_imagen = tk.Label(emojis_frame, image=imagen)
+            etiqueta_imagen = tk.Label(ventana, image=imagen)
             etiqueta_imagen.image = imagen
             etiqueta_imagen.pack(side=tk.LEFT)
 
-            texto_con_emojis = texto_con_emojis.replace(emoji_encontrado, f" {emoji_encontrado} ")
+            texto_con_emojis = texto_con_emojis.replace(emoji_encontrado, "")
+            emojis += 1
 
-    return texto_con_emojis
+    return texto_con_emojis, emojis
 
+
+def analizador_lexicográfico(entrada):
+    lista_emojis_encontrados, total_emojis = identificar_emojis(entrada)
+    entrada_con_emojis = reemplazar_emojis_con_imagenes(
+        entrada, lista_emojis_encontrados
+    )
+
+    # Actualiza la etiqueta de texto con el texto procesado
+    etiqueta_texto.config(text=entrada_con_emojis)
+
+    # Muestra el resultado en la etiqueta de salida
+    palabras_espanol = VerificadorPalabras(
+        "emoji_parser-main/diccionario.txt"
+    )
+    total_palabras = palabras_espanol.contar_palabras_espanol(entrada)
+    print(f"Se encontraron {total_palabras} palabras y {total_emojis} emojis.")
+    etiqueta_salida.config(
+        text=f"Se encontraron {total_palabras} palabras y {total_emojis} emojis."
+    )
+
+
+def presionar_boton():
+    entrada_texto = input_entry.get()
+    texto_salida = analizador_lexicográfico(entrada_texto)
+    etiqueta_salida.config(text=texto_salida)
+
+# Crear la ventana principal
 ventana = tk.Tk()
 ventana.title("Analizador de Emojis")
 ventana.geometry("800x500")
@@ -91,32 +114,43 @@ texto_adicional1 = "LENGUAJES DE PROGRAMACIÓN"
 fuente_negrita = ("Arial", "13", "bold")
 label_texto_adicional = tk.Label(ventana, text=texto_adicional, font=fuente_negrita)
 label_texto_adicional1 = tk.Label(ventana, text=texto_adicional1, font=fuente_negrita)
-label_texto_adicional.pack(anchor="ne")  
+label_texto_adicional.pack(anchor="ne")
 label_texto_adicional1.pack(anchor="ne")
 
-imagen = Image.open('./emoji_parser-main/emojicshd/logo_eafit_completo.png')
+# Imagen
+imagen = Image.open("emoji_parser-main\emojicshd\logo_eafit_completo.png")
 imagen = imagen.resize((300, 100), Image.LANCZOS)
 render = ImageTk.PhotoImage(imagen)
 imagen_label = tk.Label(ventana, image=render)
 imagen_label.pack(anchor="nw", side="top")
 
+# Frame para organizar elementos superiores
 frame_top = tk.Frame(ventana)
 frame_top.pack()
 
+# Label "Ingrese un texto" en negrita debajo de la imagen
 fuente_label = ("Arial", 14, "bold")
 label_ingrese_texto = tk.Label(frame_top, text="Ingrese un texto:", font=fuente_label)
-label_ingrese_texto.grid(row=0, column=0, padx=20, pady=20, sticky="w")  
+label_ingrese_texto.grid(
+    row=0, column=0, padx=20, pady=20, sticky="w"
+)  # Posicionamiento a la izquierda
 
+# Campo de entrada de texto al lado del texto "Ingrese un texto"
 fuente_entrada = ("Arial", 14)
 input_entry = tk.Entry(frame_top, font=fuente_entrada)
-input_entry.grid(row=0, column=1, padx=5, pady=20, sticky="w")  
+input_entry.grid(
+    row=0, column=1, padx=5, pady=20, sticky="w"
+)  # Posicionamiento a la izquierda
 
-boton = tk.Button(ventana, text="Procesar texto", command=lambda: analizador_lexicografico(input_entry.get()))
-boton.pack(pady=10)  
+# Botón "Procesar texto" debajo del campo de entrada
+boton = tk.Button(ventana, text="Procesar texto", command=presionar_boton)
+boton.pack(pady=10)  # Ajuste de espaciado
 
+# Etiqueta para mostrar el texto analizado
 etiqueta_texto = tk.Label(ventana, text="", wraplength=600)
 etiqueta_texto.pack()
 
+# Etiqueta de salida para mostrar el resultado después de procesar la cadena
 etiqueta_salida = tk.Label(ventana, text="Salida: ", font=fuente_label)
 etiqueta_salida.pack()
 
